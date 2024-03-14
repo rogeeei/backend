d34cea527ecde2f3b02a9a3477e407ddfaa512b9
rogeeei <cuevasrogelyn2@gmail.com>
2024-03-15 00:07:02 +0800

    Added User Request and Show

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -13,6 +15,21 @@ class UserController extends Controller
     public function index()
     {
         return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
+
+        return $user; 
     }
 
 
--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::delete('/carousel/{id}',[CarouselItemsController::class, 'destroy']);
 
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/{id}',[UserController::class, 'show']);
+Route::post('/user',[UserController::class, 'store']);
 Route::delete('/user/{id}',[UserController::class, 'destroy']);
 
 Route::get('/student', [StudentController::class, 'index']);
